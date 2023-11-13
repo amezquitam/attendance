8c5f55da8cfc01903c21a87399f43edba5360c41
MiguelAmezquitaM <maamezquita@unimagdalena.edu.co>
2023-11-13 14:59:52 -0500

    creating subject model

--- src/main/java/com/developers/attendance/entities/Subject.java
+++ src/main/java/com/developers/attendance/entities/Subject.java
@@ -1,9 +1,26 @@
 package com.developers.attendance.entities;
 
-import javax.persistance.Entity;
+import java.time.DayOfWeek;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table
 public class Subject {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column
     String name;
-    Teacher teacher;
+
+    @Column
+    List<DayOfWeek> dayOfWeek;
 }
